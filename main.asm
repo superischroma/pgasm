934dec8b25ec462f094ba9dcc833a4ef0eeca61f
super <awesomegamer156@gmail.com>
2021-09-06 21:37:08 -0400

    direct itoa and atoi

--- main.asm
+++ main.asm
@@ -2,13 +2,15 @@
 
 section .data
 msg: db "Hello", 0
+stri: db "-785", 0
 
 section .text
-global WinMain
-WinMain:
-    mov rcx, 2
-    call printb
-    call printnl
+global main
+main:
+    mov rcx, stri
+    call atoi
+    mov rcx, rax
+    call printi
 
     mov rcx, 0
     call exit
--- stdfunc.asm
+++ stdfunc.asm
@@ -1,3 +1,13 @@
+; --- Routine Rules ---
+; Arguments always follow the RCX, RDX, R8, R9, reverse stack order.
+; Return values are placed in the RAX register except if the routine returns void.
+; ---------------------
+
+; --- Documentation Types ---
+; string: A pointer to the first byte of a C string (a null-terminated string).
+; buffer: A pointer to the first byte of any uninitialized data.
+; ---------------------------
+
 extern GetStdHandle
 extern WriteFile
 extern ExitProcess
@@ -11,7 +21,7 @@ falsestr: db "false", 0
 
 section .bss
 outh: resq 1 ; cache for stdout handle so we only need to call GetStdHandle once
-pibuf: resb 16 ; buffer for numerical chars when printing out an integer
+itoabuf: resb 65 ; buffer for numerical chars when printing out an integer
 
 section .text
 
@@ -58,7 +68,7 @@ printi:
     mov rcx, rax
     add rdx, 48
 
-    mov rax, pibuf
+    mov rax, itoabuf
     add rax, rbx
     mov byte [rax], dl
 
@@ -77,7 +87,7 @@ printi:
 
     dec rbx
 
-    mov rax, pibuf
+    mov rax, itoabuf
     add rax, rbx
     mov byte [rax], dl
 
@@ -89,7 +99,7 @@ printi:
     call stdout
     mov rcx, rax
 
-    mov rdx, pibuf
+    mov rdx, itoabuf
     add rdx, rbx
 
     mov r8, 15
@@ -106,6 +116,123 @@ printi:
     pop rbx
     pop rax
     ret
+
+; itoa(rcx: int, rdx: buffer) -> void
+itoa:
+    push rax
+    push rbx
+    push r8
+    push r9
+    mov rbx, 64 ; reverse counter
+    cmp rcx, 0
+    jl .itoan
+    mov r9, FALSE
+    jmp .itoal
+
+.itoan:
+    imul rcx, -1
+    mov r9, TRUE
+
+.itoal:
+    push rdx
+    mov rdx, 0
+    mov rax, rcx
+    mov r8, 10
+    idiv r8
+    mov rcx, rax
+    add rdx, 48
+
+    mov rax, itoabuf
+    add rax, rbx
+    mov byte [rax], dl
+
+    pop rdx
+    cmp rcx, 0
+    je .itoanc
+    dec rbx
+    jmp .itoal
+
+.itoanc:
+    cmp r9, TRUE ; is value negative?
+    je .itoanu
+    mov r8, 0 ; = buffer index
+    jmp .itoap
+
+.itoanu:
+    dec rbx
+
+    push rdx
+    mov rdx, 45 ; negative sign
+
+    mov rax, itoabuf
+    add rax, rbx
+    mov byte [rax], dl
+    mov r8, 0
+    pop rdx
+
+.itoap:
+    mov rax, [itoabuf + rbx]
+    lea r9, [rdx + r8] ; r9 now points towards rdx + r8 (buffer + current index in buffer)
+    mov [r9], rax ;
+    inc rbx
+    inc r8
+    ; r9 is now used as a pointer
+    cmp rbx, 65 ; are we not at the highest indexed char?
+    jne .itoap ; if so, loop
+
+.itoae:
+    lea r9, [rdx + r8]
+    mov [r9], byte 0
+    pop r9
+    pop r8
+    pop rbx
+    pop rax
+    ret
+
+; atoi(rcx: string) -> rax: int
+atoi:
+    push rbx
+    push rdx
+    push rdi
+    push rsi
+    call strlen
+    mov rdx, rax ; iterator
+    mov rax, 0 ; final value
+    mov rbx, 1 ; multiplier
+    cmp byte [rcx], 45 ; is our string number negative?
+    je .atoin ; if so, mark it
+    mov rdi, FALSE ; otherwise, mark that it is not
+    jmp .atoil ; start loop
+
+.atoin:
+    mov rdi, TRUE
+
+.atoil:
+    dec rdx
+    mov rsi, 0
+    mov sil, byte [rcx + rdx] ; move our current value into r9
+    sub sil, 48 ; get its true value
+    imul rsi, rbx ; multiply by the multiplier
+    imul rbx, 10 ; multiply the multiplier by 10
+    add rax, rsi ; add it to our final value
+    cmp rdx, rdi ; are we at the end of the string? (1 for negatives, 0 for positives)
+    je .atoinc ; if so, jump to the negativity check
+    jmp .atoil ; otherwise, continue the loop
+
+.atoinc:
+    cmp rdi, TRUE ; is our string number negative?
+    je .atoimkn ; if so, make it negative
+    jmp .atoie ; otherwise, jump to ending pops
+
+.atoimkn:
+    imul rax, -1 ; make the value negative
+
+.atoie:
+    pop rsi ; pop off reserved values
+    pop rdi
+    pop rdx
+    pop rbx
+    ret ; return to caller
 
 ; printb(rcx: bool) -> void
 printb:
